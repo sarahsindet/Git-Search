c9e7090a0d537a04683794ca4fcd0f0b3b334dcb
sarahsindet <sarahsindet@gmail.com>
2020-07-01 15:37:55 +0300

    added some components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { RepoComponent } from './repo/repo.component';
+import { GitComponent } from './git/git.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    NotFoundComponent,
+    RepoComponent,
+    GitComponent
   ],
   imports: [
     BrowserModule,
